83c501660b9a75e3c3c120d13c2f1b57b75f01a6
Wesley.Wang <wesley.wang@zerologix.com>
2018-11-08 13:36:26 +1100

    first commit

---
+++ Containers/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Home from './Home'
+import './styles/index.scss'
+class App extends Component {
+    render() {
+        console.log('enter')
+        return (
+            <div>
+                <h1>My React App 111 123  aaa!</h1>
+                <Home />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ Containers/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './styles/home.scss'
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <p className='text'>Home</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
